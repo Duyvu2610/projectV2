bfc33e98e92716a4be4b2ce80d1bf1255676b218
viet0103 <nguyenvanhoang11446@gmail.com>
2023-04-16 13:06:42 +0700

    complete save a file in current graph and 50% open file

--- view/FileView.java
+++ view/FileView.java
@@ -25,12 +25,44 @@ public class FileView extends JPanel {
 	public FileView(GraphController controller) {
 		this.controller = controller;
 		setLayout(new FlowLayout(FlowLayout.LEADING));
+		// save file
 		saveFileButton.setPreferredSize(new Dimension(120, 60));
 		saveFileButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				// create matrix (String type)
+				int[][] matrix = controller.getGraph().getAdjacencyMatrix();
+				int sizeMatrix = matrix.length;
+				String rowString = "";
+				String result = "";
 
+				for (int row = 0; row < sizeMatrix; row++) {
+					for (int col = 0; col < sizeMatrix; col++) {
+						if (col == sizeMatrix-1) {
+							rowString += String.valueOf(matrix[row][col]);
+						} else {
+							rowString += String.valueOf(matrix[row][col]) + " ";
+						}
+					}
+					if (row == sizeMatrix-1) {
+						result += rowString;
+					} else {
+						result += rowString +"\n";
+					}
+					rowString = "";
+				}
 
+				JFileChooser fileChooser = new JFileChooser();
+				int chooser = fileChooser.showSaveDialog(null);
+				if (chooser == JFileChooser.APPROVE_OPTION) {
+					try {
+						FileWriter fw = new FileWriter(fileChooser.getSelectedFile() + ".txt");
+						fw.write(result);
+						fw.close();
+					} catch (Exception e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
 
 		});
@@ -39,8 +71,11 @@ public class FileView extends JPanel {
 		openFileButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
-
+				JFileChooser fileChooser = new JFileChooser();
+				int chooser = fileChooser.showOpenDialog(null);
+				if (chooser == JFileChooser.APPROVE_OPTION) {
+					// Khởi tạo ma trận đã có từ file vừa mở
+				}
 			}
 		});
 
